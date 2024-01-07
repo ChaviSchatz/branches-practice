230b40064f9abf8da3df46268fc1c96628857def
מורה הנדסאים < @CM>
2024-01-07 14:12:49 +0200

    create student class

---
+++ ConsoleApp1/ConsoleApp1/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Student
+    {
+        public string? Name { get; set; }
+        public int Id { get; set; }
+
+        public Student(string name, int id)
+        {
+            Name = name;
+            Id = id;
+        }
+    }
+}
